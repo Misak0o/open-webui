120716813564879cd808ee9602ec6ff562caf1a0
Arnaud Baillif <arnaud.baillifpro@gmail.com>
2025-05-22 09:41:58 +0200

    Adding test page for the term before the refactorisation and adding a shell container that still needs to be hardened

--- src/routes/term/+page.svelte
+++ src/routes/term/+page.svelte
@@ -2,6 +2,7 @@
   import Terminal from '$lib/components/common/Terminal.svelte';
 </script>
 
-<h1 class="text-xl font-bold mb-4">Test du composant CommandBlock</h1>
+<h1 class="text-xl font-bold mb-4">Test du composant Terminal</h1>
 
 <Terminal command="ls" canRun={true} />
+<Terminal command="test" canRun={true} />
