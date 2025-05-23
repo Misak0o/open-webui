9b7d3e49740465968368350f07d08d8207091383
Arnaud Baillif <abaillif@enseirb-matmeca.fr>
2025-05-23 16:05:22 +0200

    removing my test route for the terminal

--- src/routes/term/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Terminal from '$lib/components/common/Terminal.svelte';
-</script>
-
-<h1 class="text-xl font-bold mb-4">Test du composant Terminal</h1>
-
-<Terminal command="ls" canRun={true} />
-<Terminal command="test" canRun={true} />
